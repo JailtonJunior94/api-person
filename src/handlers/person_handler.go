efe53c426cd9ff307f13503f5d90469c1287cdd2
jailton.junior <jailton.junior@iterative.com.br>
2020-10-07 15:50:47 -0300

    Testando o lint e Docker publish

--- src/handlers/person_handler.go
+++ src/handlers/person_handler.go
@@ -12,16 +12,16 @@ import (
 func ListPersonHandle(c *fiber.Ctx) {
 	res, err := infra.ListPersons()
 	if err != nil {
-		c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível listar pessoas!"})
+		_ = c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível listar pessoas!"})
 		return
 	}
 
 	if len(res) == 0 {
-		c.Status(404).JSON(models.Response{Success: false, Message: "Não foi encontrado nenhuma pessoa!"})
+		_ = c.Status(404).JSON(models.Response{Success: false, Message: "Não foi encontrado nenhuma pessoa!"})
 		return
 	}
 
-	c.Status(200).JSON(res)
+	_ = c.Status(200).JSON(res)
 }
 
 // GetByIDPersonHandle - handle para recuperar pessoa por Id
@@ -29,30 +29,30 @@ func GetByIDPersonHandle(c *fiber.Ctx) {
 	id := c.Params("id")
 	res, err := infra.GetPersonByID(id)
 	if err != nil {
-		c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível encontrar a pessoa informada"})
+		_ = c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível encontrar a pessoa informada"})
 		return
 	}
 
 	if res == nil {
-		c.Status(404).JSON(models.Response{Success: false, Message: "Não foi possível encontrar a pessoa informada"})
+		_ = c.Status(404).JSON(models.Response{Success: false, Message: "Não foi possível encontrar a pessoa informada"})
 		return
 	}
 
-	c.Status(200).JSON(res)
+	_ = c.Status(200).JSON(res)
 }
 
 // NewPersonHandle - handle para adicionar nova pessoa
 func NewPersonHandle(c *fiber.Ctx) {
 	body := models.Person{}
-	json.Unmarshal([]byte(c.Body()), &body)
+	_ = json.Unmarshal([]byte(c.Body()), &body)
 
 	newPerson, err := infra.CreatePerson(body)
 	if err != nil {
-		c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível cadastrar pessoa"})
+		_ = c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível cadastrar pessoa"})
 		return
 	}
 
-	c.Status(201).JSON(newPerson)
+	_ = c.Status(201).JSON(newPerson)
 }
 
 // UpdatePersonHandle - handle para atualizar dados da pessoa
@@ -60,20 +60,20 @@ func UpdatePersonHandle(c *fiber.Ctx) {
 	id := c.Params("id")
 	body := models.Person{}
 
-	json.Unmarshal([]byte(c.Body()), &body)
+	_ = json.Unmarshal([]byte(c.Body()), &body)
 
 	update, err := infra.UpdatePerson(id, body)
 	if err != nil {
-		c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível atualizar pessoa"})
+		_ = c.Status(400).JSON(models.Response{Success: false, Message: "Não foi possível atualizar pessoa"})
 		return
 	}
 
 	if update == nil {
-		c.Status(404).JSON(models.Response{Success: false, Message: "Não foi possível atualizar pessoa"})
+		_ = c.Status(404).JSON(models.Response{Success: false, Message: "Não foi possível atualizar pessoa"})
 		return
 	}
 
-	c.Status(200).JSON(update)
+	_ = c.Status(200).JSON(update)
 }
 
 // DeletePersonHandle - handle para remover cadastro da pessoa
@@ -81,14 +81,14 @@ func DeletePersonHandle(c *fiber.Ctx) {
 	id := c.Params("id")
 	line, err := infra.DeletePerson(id)
 	if err != nil {
-		c.Status(400).JSON(models.Response{Success: false, Message: "Erro ao deletar pessoa"})
+		_ = c.Status(400).JSON(models.Response{Success: false, Message: "Erro ao deletar pessoa"})
 		return
 	}
 
 	if line == 0 {
-		c.Status(404).JSON(models.Response{Success: false, Message: "Erro ao deletar pessoa"})
+		_ = c.Status(404).JSON(models.Response{Success: false, Message: "Erro ao deletar pessoa"})
 		return
 	}
 
-	c.Status(200).JSON(models.Response{Success: true, Message: "Pessoa deletada com sucesso"})
+	_ = c.Status(200).JSON(models.Response{Success: true, Message: "Pessoa deletada com sucesso"})
 }
