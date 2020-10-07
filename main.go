cfa25039b37f9e1a1c0a36cb91dcfa5d2ba57ec7
jailton.junior <jailton.junior@iterative.com.br>
2020-10-07 14:07:43 -0300

    Ajustes nos arquivos

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"samplegofiber/src/routes"
 
-	swagger "github.com/arsmn/fiber-swagger"
-	_ "github.com/arsmn/fiber-swagger/v2/example/docs"
 	"github.com/gofiber/fiber"
 	"github.com/subosito/gotenv"
 )
@@ -19,12 +17,6 @@ func main() {
 		c.Send("Seja bem-vindo(a) a API Golang + Fiber + Swagger + MongoDB + Docker")
 	})
 	routes.PersonRouter(app)
-
-	app.Use("/swagger", swagger.Handler)
-	app.Use("/swagger", swagger.New(swagger.Config{
-		URL:         "http://example.com/doc.json",
-		DeepLinking: false,
-	}))
 
 	println("Running on http://0.0.0.0/3000")
 	app.Listen(3000)
